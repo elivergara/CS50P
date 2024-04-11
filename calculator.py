9228918d2d9d4c32ce2d12a974490f7e8bf574de
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2024-04-11 18:19:55 +0000

    Thu, Apr 11, 2024, 11:19 AM -07:00

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
-x = int(input("What's X "))
-y = input("What's Y ")
+# x = int(input("What's X "))
+# y = input("What's Y ")
 
-z = x+int(y)
+# z = x+int(y)
 
-print(z)
+# print(z)
 
 
-# Improved
-x = int(input("What's X "))
-y = int(input("What's Y "))
+# # Improved
+# x = int(input("What's X "))
+# y = int(input("What's Y "))
+# print(x+y)
+
+# # Alternative though not necessarily improved
+# print(int(input("What's X "))+int(input("What's Y ")))
+
+# Using float
+x = float(input("What's X "))
+y = float(input("What's Y "))
 print(x+y)
-
-# Alternative though not necessarily improved
-print(int(input("What's X "))+int(input("What's Y ")))
 
