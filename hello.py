0c60eba9dd51962359114e692e61ca267efdaaa0
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2024-04-11 17:13:46 +0000

    Thu, Apr 11, 2024, 10:13 AM -07:00

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # Ask user for their name
-name = input("What's your name? ").strip().title() #Input expects text
+name = input("What's your name? ").strip().title()  # Input expects text
 
 # name=name.strip() #Remove whitespace from str (Function assigned to variable, right to left)
 # name=name.capitalize() #capitalize (only the first letter in the string)
@@ -7,9 +7,12 @@ name = input("What's your name? ").strip().title() #Input expects text
 # name.strip().title()  # capitalize all words at once
 # See top line for final line
 # Say hello to user
+
+
 def greeting():
     print("Hello, " + name)  # Needs to have a space after "Hello,"
-    print("Hello,", name, "How are you?", end="")  # Does not need to have a space after "Hello,". Adds it automatically.
+    # Does not need to have a space after "Hello,". Adds it automatically.
+    print("Hello,", name, "How are you?", end="")
     # end=\n is the default.
     print(f"Hello, {name}", "What's up?", sep="*")  # Format String. Default is a space " _"
     print("Hello, \"Friend\"")  # ESACAPING: using \
@@ -17,5 +20,5 @@ def greeting():
     first, last = name.split(" ")
     print(f"Hola, {first}")
 
-greeting()
 
+greeting()
