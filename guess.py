7ec85467588b6d7ceba3ae32186340daaa39b91a
Eli Vergara <elivergara@gmail.com>
2024-04-12 11:15:01 -0700

    updates lesson 2

--- guess.py
+++ guess.py
@@ -1,11 +1,14 @@
+
 # Learn the difference between a str and an int as an input
 def get_guess():
-    guess = int(input("Guess a number: "))
+  guess = input("Guess a number: ")
     if guess == 50:
         print("Correct!")
     else:
-        print("Wrong!")
+        print(Wrong!")
         return get_guess()
 
 print(get_guess())
+print(get_guess())
+            
             
--- src0/src0/hello1.py
+++ src0/src0/hello1.py
@@ -3,3 +3,10 @@
 name = input("What's your name? ")
 print("hello,")
 print(name)
+
+def hello():
+    return "Hello, World!"
+
+def say_hello():
+  return "Hello, World!"
+
